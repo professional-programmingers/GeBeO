719e500db46c60da39a3a3bfcdcdaf340dbc1e05
Jonathan Alexander <me@jmalexan.com>
2017-10-02 20:56:20 -0700

    Move images to a less hard-coded solution, will add image adding soon.

--- GeBeO.py
+++ GeBeO.py
@@ -1,14 +1,13 @@
-import sys
 from datetime import datetime
 import asyncio
 from random import randint
 import pytz
 import discord
 import emojitable
-from google.cloud import datastore
 import time
 from cowpy import cow
 import re
+import os
 
 expanding_channels = None
 
@@ -17,8 +16,6 @@ discord_token = f.read().strip()
 f.close()
 
 client = discord.Client()
-
-dsclient = datastore.Client(project="grillbybot")
 
 
 async def wednesday_detector():
@@ -35,7 +32,9 @@ async def wednesday_detector():
             if not currently_wednesday:
                 currently_wednesday = True
                 my_dudes = "<:MyDudes:304341572168712193> "
-                await client.send_message(client.get_channel('137685095111720961'), my_dudes * 3 + "It is Wednesday my dudes" + my_dudes * 3)
+                chan = client.get_channel('137685095111720961')
+                msg = my_dudes * 3 + "It is Wednesday my dudes" + my_dudes * 3
+                await client.send_message(chan, msg)
         else:
             currently_wednesday = False
         await asyncio.sleep(60)
@@ -55,13 +54,14 @@ async def timer(duration, length, channel):
             text = "```diff\n-"
         elif counter <= float(duration)*2/3:
             text = "```fix\n "
-        else: 
+        else:
             text = "```diff\n+"
         # Countup counter
         text += "  " + str(counter) + " |"
         # Progress bar calculation
         progress = int(round(((float(counter)/duration*length))))
-        text += "-"*progress + " "*(length-progress) + "| " + str(duration) + "\n```"
+        strdur = str(duration)
+        text += "-"*progress + " "*(length-progress) + "| " + strdur + "\n```"
         await client.edit_message(message, text)
         counter += 1
     await client.add_reaction(message, u"\U000023F0")
@@ -86,29 +86,17 @@ async def on_ready():
     client.loop.create_task(wednesday_detector())  # Start up wednesay detector
     print('------')
 
+
 @client.event
 async def on_message(message):
     print(message.author.name + ": " + message.content)
     message_split = message.content.split(' ')
     command = message_split[0]
-    args_split = message_split[1:]  # Args split into multiple (cannot have space as an argument)
+    # Args split into multiple (cannot have space as an argument)
+    args_split = message_split[1:]
     arg = ' '.join(args_split)  # One arg with spaces.
 
-
-    if command in ['!test']:
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-
-    elif command in ['!sleep']:
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-
-    elif command in ['!ayy']:
-        print("TEST!!!")
+    if command in ['!ayy']:
         nickname = message.server.me.nick
         await client.delete_message(message)
         await client.change_nickname(message.server.me, "ayy")
@@ -124,7 +112,8 @@ async def on_message(message):
 
     elif command in ['!blowme']:
         await client.delete_message(message)
-        await client.send_message(message.channel, "*sucks " + message.author.nick + " off*")
+        nick = message.author.nick
+        await client.send_message(message.channel, "*sucks " + nick + " off*")
 
     elif command in ['!esad']:
         await client.delete_message(message)
@@ -136,17 +125,16 @@ async def on_message(message):
 
     elif command in ['!cowsay']:
         await client.delete_message(message)
-        async for message_to_edit in client.logs_from(message.channel, limit=1, before=message):
+        messages = client.logs_from(message.channel, limit=1, before=message)
+        async for message_to_edit in messages:
             to_edit = message_to_edit.content
-            edited = '```' + re.sub('```', '', cow.milk_random_cow(to_edit)) + '```'
+            random_cow = cow.milk_random_cow(to_edit)
+            edited = '```' + re.sub('```', '', random_cow) + '```'
             await client.send_message(message.channel, edited)
 
     elif command in ['!doubt', '!suspect']:
         await client.delete_message(message)
         await client.send_file(message.channel, "assets/doubt.png")
-
-    elif command in ['!fullstop']:
-        sys.exit()
 
     elif command in ['!react'] and arg:
         last_message = []
@@ -158,7 +146,8 @@ async def on_message(message):
 
     elif command in ['!retard', '!rt']:
         await client.delete_message(message)
-        async for message_to_edit in client.logs_from(message.channel, limit=1, before=message):
+        messages = client.logs_from(message.channel, limit=1, before=message)
+        async for message_to_edit in messages:
             to_edit = message_to_edit.content
             edited = ''
             for char in to_edit:
@@ -168,7 +157,8 @@ async def on_message(message):
 
     elif command in ['!mock']:
         await client.delete_message(message)
-        async for message_to_edit in client.logs_from(message.channel, limit=1, before=message):
+        messages = client.logs_from(message.channel, limit=1, before=message)
+        async for message_to_edit in messages:
             to_edit = message_to_edit.content
             edited = ''
             for char in to_edit:
@@ -187,5 +177,17 @@ async def on_message(message):
             length = int(args_split[1])
             client.loop.create_task(timer(duration, length, message.channel))
 
-#if __name__ == "__main__":
+    elif command in ['!i']:
+        await client.delete_message(message)
+        if len(args_split) == 0:
+            listofimages = ""
+            for img in os.listdir("images"):
+                listofimages += img + "\n"
+            await client.send_message(message.channel, listofimages)
+        elif len(args_split) == 1:
+            for img in os.listdir("images"):
+                if img.split(".")[0] == args_split[0]:
+                    await client.send_file(message.channel, "images/" + img)
+
+
 client.run(discord_token)
