26b3b0ec703e3b7c2b6911b315c8dd99fe62d1d6
Jonathan Alexander <me@jmalexan.com>
2018-01-10 23:45:46 -0800

    Implement auto expando channels

--- cogs/expandochannels.py
+++ cogs/expandochannels.py
@@ -1,62 +1,117 @@
+import os
+import json
 import discord
 from discord.ext import commands
 
 class ExpandoChannels():
     def __init__(self, bot : commands.Bot):
         print("initializing expandochannels")
+        if not os.path.exists("cache"):
+            os.makedirs("cache")
+        if os.path.isfile("cache/expandochannel.txt") and os.stat("cache/expandochannel.txt").st_size != 0:
+            self.expando_channel_list = json.loads(open("cache/expandochannel.txt", "r").read())
         self.bot = bot
 
+    expando_channel_list = []
+
+    async def on_ready(self):
+        self.update_expando_channel_list()
+
+    def update_expando_channel_list(self):
+        todelete = []
+        for expando_channel in self.expando_channel_list:
+            channel = self.bot.get_channel(expando_channel["chan_id"])
+            if channel is None:
+                todelete.append(expando_channel)
+        self.expando_channel_list[:] = [r for r in self.expando_channel_list if r not in todelete]
+        expando_channel_cache = open("cache/expandochannel.txt", "w")
+        expando_channel_cache.write(json.dumps(self.expando_channel_list))
+        expando_channel_cache.close()
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def expando(self, ctx):
+        if ctx.author.voice is not None:
+            vchan = ctx.author.voice.channel
+            for ec in self.expando_channel_list:
+                if ec["chan_id"] == vchan.id:
+                    await ctx.send("That channel is already an expando channel!")
+                    return
+            expandochan = {}
+            expandochan["chan_id"] = vchan.id
+            expandochan["perm"] = True
+            self.expando_channel_list.append(expandochan)
+            await self.update_empty_channel(vchan)
+        else:
+            await ctx.send("Join the channel you wanna turn into a source expando channel!")
+
     async def update_empty_channel(self, starting_channel):
-        vclist = starting_channel.guild.voice_channels
-        starting_name = "#".join(starting_channel.name.split("#")[:-1])
-        counter = 0
-        lastvc = None
-        looking_for_pos = True
-        put_after_this_vc = None
-        origin_is_empty = False
-        found_channels = False
-        for vc in vclist:
-            if "#" in vc.name:
-                split_name = vc.name.split("#")
-                current_name = "#".join(split_name[:-1])
-                if starting_name == current_name:
-                    found_channels = True
-                    current_position = int(split_name[-1])
-                    if len(vc.members) == 0:
-                        if current_position == 1:
-                            origin_is_empty = True
-                        else:
-                            await vc.delete()
-                        continue
-                    if looking_for_pos:
-                        counter += 1
-                        if current_position > counter:
-                            looking_for_pos = False
-                            put_after_this_vc = lastvc.id
+        chan_index = starting_channel.guild.voice_channels.index(starting_channel)
+        last_pos = 0
+        perm_is_empty = False
+        for vc in starting_channel.guild.voice_channels[chan_index:]:
+            expando_chan = None
+            for ec in self.expando_channel_list:
+                if ec["chan_id"] == vc.id:
+                    expando_chan = ec
+                    break
+            if starting_channel == vc and expando_chan["perm"]:
+                last_pos = vc.position
+                if len(vc.members) == 0:
+                    perm_is_empty = True
+                continue
+            if expando_chan is not None and not expando_chan["perm"]:
+                if len(vc.members) == 0:
+                    await vc.delete()
+                    self.expando_channel_list.remove(ec)
                 else:
-                    if found_channels:
-                        break
-                lastvc = vc
-        if not origin_is_empty:
-            if looking_for_pos:
-                counter += 1
-                put_after_this_vc = lastvc.id
-            thevc = self.bot.get_channel(put_after_this_vc)
-            new_empty_channel = await lastvc.guild.create_voice_channel(starting_name + "#" + str(counter), category=thevc.category)
-            new_position = thevc.position + 1
-            await new_empty_channel.edit(position = new_position)
+                    last_pos = vc.position
+            else:
+                break
+        if not perm_is_empty:
+            new_chan = await starting_channel.guild.create_voice_channel(self.bot.config["default_channel_name"], category=starting_channel.category)
+            try:
+                await new_chan.edit(position=last_pos + 1)
+            except discord.errors.InvalidArgument:
+                pass
+            expandochan = {}
+            expandochan["chan_id"] = new_chan.id
+            expandochan["perm"] = False
+            self.expando_channel_list.append(expandochan)
+        expando_channel_cache = open("cache/expandochannel.txt", "w")
+        expando_channel_cache.write(json.dumps(self.expando_channel_list))
+        expando_channel_cache.close()
+
 
     async def on_voice_state_update(self, member, before: discord.VoiceState, after: discord.VoiceState):
-        if before.channel is not None and after.channel is not None:
-            if before.channel.name.split("#")[:-1] == after.channel.name.split("#")[:-1]:
-                await self.update_empty_channel(before.channel)
-                return
-        if before.channel is not None:
-            if "#" in before.channel.name:
-                await self.update_empty_channel(before.channel)
-        if after.channel is not None:
-            if "#" in after.channel.name:
-                await self.update_empty_channel(after.channel)
+        #Go through the expando channels, saving the source channels for the before and after channels
+        before_starting_point = None
+        after_starting_point = None
+        last_perm_id = 0
+        for vc in member.guild.voice_channels:
+            expando_channel = None
+            for ec in self.expando_channel_list:
+                if ec["chan_id"] == vc.id:
+                    expando_channel = ec
+                    break
+            if expando_channel is not None:
+                if expando_channel["perm"]:
+                    last_perm_id = expando_channel["chan_id"]
+                if before.channel is not None:
+                    if expando_channel["chan_id"] == before.channel.id:
+                        before_starting_point = member.guild.get_channel(last_perm_id)
+                if after.channel is not None:
+                    if expando_channel["chan_id"] == after.channel.id:
+                        after_starting_point = member.guild.get_channel(last_perm_id)
+
+        #If both starting points are the same, update the empty channel once, otherwise update according to the individual ones.
+        if after_starting_point == before_starting_point:
+            await self.update_empty_channel(before_starting_point)
+        else:
+            if after_starting_point is not None:
+                await self.update_empty_channel(after_starting_point)
+            if before_starting_point is not None:
+                await self.update_empty_channel(before_starting_point)
 
 def setup(bot : commands.Bot):
     print("setting up expandochannels")
