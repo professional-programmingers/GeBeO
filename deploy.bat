182c73860ed96783fa1e6ef68747b937fd435e58
Jonathan Alexander <me@jmalexan.com>
2018-04-21 03:04:07 -0700

    Improve on the deploy script a bit

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,4 @@
 docker build -t professionalprogrammingers/gebeo .
 docker push professionalprogrammingers/gebeo
 
-ssh gebeo@gebeo.jmalexan.com sudo docker stop gebeo
-ssh gebeo@gebeo.jmalexan.com sudo docker rm gebeo
-ssh gebeo@gebeo.jmalexan.com sudo docker pull professionalprogrammingers/gebeo
-ssh gebeo@gebeo.jmalexan.com sudo docker run -v ~/volumes/tokens:/app/tokens -v ~/volumes/guilds:/app/guilds --name gebeo -d --rm professionalprogrammingers/gebeo
+ssh -t gebeo@gebeo.jmalexan.com 'sudo docker stop gebeo; sudo docker rm gebeo; sudo docker pull professionalprogrammingers/gebeo; sudo docker run -v ~/volumes/tokens:/app/tokens -v ~/volumes/guilds:/app/guilds --name gebeo -d --rm professionalprogrammingers/gebeo'
