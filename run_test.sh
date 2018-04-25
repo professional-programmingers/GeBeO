d3357b704870f936a3af176a9dbe34151c193eb8
Jonathan Alexander <me@jmalexan.com>
2018-04-25 03:27:35 -0700

    Make shell script versions of batch scripts

---
+++ run_test.sh
@@ -0,0 +1,4 @@
+sudo docker kill gebeo
+sudo docker rm gebeo
+sudo docker build -t professionalprogrammingers/gebeo .
+sudo docker run -v ~/Programming/GeBeO/tokens:/app/tokens -v ~/Programming/GeBeO/guilds:/app/guilds --name gebeo -d --rm professionalprogrammingers/gebeo
