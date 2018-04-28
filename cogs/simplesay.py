2d45fb0fb4ed99ac536e8239d73d4de717972efd
Jonathan Alexander <me@jmalexan.com>
2018-04-28 00:28:44 -0700

    Add !roll command

--- cogs/simplesay.py
+++ cogs/simplesay.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import asyncio
+import random
 
 class SimpleSay():
     def __init__(self, bot):
@@ -45,7 +46,7 @@ class SimpleSay():
                 await ctx.send("Please mention a user to get the avatar of!")
             else:
                 await ctx.send(ctx.message.mentions[0].avatar_url)
-    
+
     @commands.command()
     async def disapprove(self, ctx):
         """
@@ -54,6 +55,38 @@ class SimpleSay():
         await ctx.trigger_typing()
         await ctx.send("ಠ_ಠ")
         await ctx.message.delete()
+
+    @commands.command()
+    async def roll(self, ctx):
+        rollstrs = []
+        sum = 0
+        for s in ctx.args_split:
+            if "d" in s:
+                dice_split = s.split("d")
+                if len(dice_split) == 2:
+                    try:
+                        numdice = int(dice_split[0])
+                        numsides = int(dice_split[1])
+                        if numdice > 0 and numsides > 0:
+                            rollstr = s + ": "
+                            for i in range(0, numdice):
+                                roll = random.randint(1, numsides)
+                                sum += roll
+                                rollstr += str(roll)
+                                if not i == numdice - 1:
+                                    rollstr += ", "
+                            rollstrs.append(rollstr)
+                    except:
+                        pass
+        if len(rollstrs) > 0:
+            msg = "`roll"
+            if not len(rollstrs) == 1:
+                msg += "s"
+            msg += ":`\n"
+            for rs in rollstrs:
+                msg += "`" + rs + "`\n"
+            msg += "`sum: " + str(sum) + "`"
+            await ctx.send(msg)
 
 def setup(bot):
     print("setting up simplesay")
