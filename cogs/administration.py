f664957f86c145c39db9f5df6f1c4c13e0ce6f1c
Jonathan Alexander <me@jmalexan.com>
2018-04-27 20:46:42 -0700

    Fix edit command

--- cogs/administration.py
+++ cogs/administration.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 import asyncio
 
 class Administration():
@@ -11,13 +12,14 @@ class Administration():
     async def edit(self, ctx):
         if not len(ctx.args_split) == 0:
             try:
-                msgid = int(ctx.args_split)
+                msgid = int(ctx.args_split[0])
                 msg = await ctx.get_message(msgid)
-                await msg.edit(content: " ".join(ctx.args_split[1:]))
+                await msg.edit(content=" ".join(ctx.args_split[1:]))
+                await ctx.message.delete()
             except ValueError:
-                await ctx.send("That's not a valid message id!", delete_after: 10)
-            except NotFound:
-                await ctx.send("Can't find that message!", delete_after: 10)
+                await ctx.send("That's not a valid message id!", delete_after=10)
+            except discord.NotFound:
+                await ctx.send("Can't find that message!", delete_after=10)
 
 
 def setup(bot):
