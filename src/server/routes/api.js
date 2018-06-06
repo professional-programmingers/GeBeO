45b974f87bb76284d1f2ab092d556eb3c51b507f
Jonathan Alexander <me@jmalexan.com>
2018-06-06 14:56:46 -0700

    get repo ready for gitlab CI and move ts compilation to dist

--- src/server/routes/api.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-exports.router = router;
-//# sourceMappingURL=api.js.map
